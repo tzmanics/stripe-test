564b9a0b618c527a7233f60beb7cd01f4f110017
Tara Z. Manicsic <tzmanics@gmail.com>
2021-06-13 23:07:00 -0400

    adds and imports products.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import products from './products.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'stripe-test';
+  products: any = products;
 }
