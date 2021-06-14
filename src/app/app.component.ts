eee22568170957babbab1189e9d840fa5a9af210
Tara Z. Manicsic <tzmanics@gmail.com>
2021-06-14 12:58:35 -0400

    builds out stripe checkout call and opening functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
 import products from './products.json';
 
 @Component({
@@ -8,4 +10,25 @@ import products from './products.json';
 })
 export class AppComponent {
   products: any = products;
+  private response: any;
+
+  constructor(private http: HttpClient) {}
+
+  async triggerCreateCheckout(eventProduct: any) {
+    this.response = await this.http
+      .post('/.netlify/functions/createCheckout', eventProduct, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .toPromise();
+    this.openStripe(this.response);
+  }
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    });
+  };
 }
